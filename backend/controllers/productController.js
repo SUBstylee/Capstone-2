11c9f0b665b87653057b8baa14be70b1de4d1825
SUBstylee <jjthrelfall@gmail.com>
2022-03-15 06:58:37 +0800

    customer reviews customers can leave reviews, limit one per product customer must have purchased product added morgan to log requests in development env

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 import Product from '../models/productModel.js';
 import asyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
 
 // @desc   Fetch all products
 // @route  GET/api/products
@@ -77,4 +78,47 @@ const updateProduct = asyncHandler(async (req, res) => {
     };
 });
 
-export { getProducts, getProductById, deleteProduct, createProduct, updateProduct };
+// @desc   Create a new review
+// @route  POST/api/products:id/reviews
+// @access Private
+const createProductReview = asyncHandler(async (req, res) => {
+    const { rating, comment } = req.body;
+    const product = await Product.findById(req.params.id);
+    if (product) {
+        //check if user has already reviewed this item
+        const alreadyReviewed = product.reviews.find(rev => rev.user.toString() === req.user._id.toString());
+        //check customer order to make sure they have purchased this item
+        const orders = await Order.find({ user: req.user._id });
+        const ordersItems = [].concat.apply(
+            [],
+            orders.map(order => order.orderItems.map(item => item.product.toString()))
+        );
+        if (product) {
+            const hasPurchased = ordersItems.includes(product._id.toString());
+            if (!hasPurchased) {
+                res.status(400);
+                throw new Error('You can only review items you have purchased!');
+            };
+        };
+        if (alreadyReviewed) {
+            res.status(400);
+            throw new Error('You can only review each product once!');
+        };
+        const review = {
+            name: req.user.name,
+            rating: Number(rating),
+            comment,
+            user: req.user._id,
+        };
+        product.reviews.push(review);
+        product.numReviews = product.reviews.length;
+        product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+        await product.save();
+        res.status(201).json({ message: 'Review added!' });
+    } else {
+        res.status(404);
+        throw new Error('Product not found!');
+    };
+});
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview };
