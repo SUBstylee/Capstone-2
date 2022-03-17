330bd5b777b97262c901775849290ce784f141d6
SUBstylee <jjthrelfall@gmail.com>
2022-03-18 04:18:25 +0800

    added loader to home page i will add a most popular items carousel to bottom of page likely will do this with product list, so for now will use loading state from that

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import Directory from "../../components/Directory/Directory";
+import Loader from "../../components/Loader/Loader";
+import Message from "../../components/Message/Message";
+import { useSelector } from "react-redux";
 const Home = () => {
+  const productList=useSelector(state=>state.productList);
+  const {loading,error}=productList;
 
   return (
     <div className='Home'>
       <h1>Welcome to Totally Awesome Apparel!</h1>
-      <Directory/>
+      {loading?
+      (<Loader/>):error?
+      (<Message variant='danger'>{error}</Message>):(<Directory/>)}
+      
     </div>
   );
 };
