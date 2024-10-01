e5698c1cd5ff748bea6632ca55aa27d14c2d81fe
SUBstylee <jjthrelfall@gmail.com>
2024-10-01 15:10:47 +0800

    added demo user and admin credentials

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,59 +1,91 @@
-import { useState,useEffect } from "react";
-import { Link,useNavigate,useLocation } from "react-router-dom";
-import {Form,Button,Row,Col} from 'react-bootstrap';
-import { useDispatch,useSelector } from "react-redux";
-import Message from "../../components/Message/Message";
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../../components/Message/Message';
 import Loader from '../../components/Loader/Loader';
-import CustomInput from "../../components/CustomInput/CustomInput";
-import {login} from '../../actions/userActions';
-import MetaWrapper from "../../components/MetaWrapper/MetaWrapper";
+import CustomInput from '../../components/CustomInput/CustomInput';
+import { login } from '../../actions/userActions';
+import MetaWrapper from '../../components/MetaWrapper/MetaWrapper';
 
-import FormContainer from "../../components/FormContainer/FormContainer";
+import FormContainer from '../../components/FormContainer/FormContainer';
 
 const Login = () => {
-    const [email,setEmail]=useState('');
-    const [password,setPassword]=useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-    const navigate = useNavigate();
-    const location = useLocation();
-    const dispatch=useDispatch();
-    const userLogin=useSelector((state)=>state.userLogin);
-    const {loading,error,userInfo}=userLogin;
-    const redirect = location.search ? location.search.split('=')[1] : '/';
+	const navigate = useNavigate();
+	const location = useLocation();
+	const dispatch = useDispatch();
+	const userLogin = useSelector((state) => state.userLogin);
+	const { loading, error, userInfo } = userLogin;
+	const redirect = location.search ? location.search.split('=')[1] : '/';
 
-    useEffect(()=>{
-        if(userInfo){
-            navigate(redirect);
-        };
-    },[navigate,userInfo,redirect]);
-    
-    const submitHandler=(e)=>{
-        e.preventDefault();
-        dispatch(login(email, password));
-    };
+	useEffect(() => {
+		if (userInfo) {
+			navigate(redirect);
+		}
+	}, [navigate, userInfo, redirect]);
 
-  return (
-    <div className="Login">
-        <FormContainer>
-            <h1>Sign In</h1>
-            {error&&<Message variant='danger'>{error}</Message>}
-            {loading&&<Loader/>}
-            <MetaWrapper title='TAA-Login'/>
-            <Form onSubmit={submitHandler}>
-                <CustomInput id='email' type='email' autoComplete='current-email' name='email' labelText='Email Address' required={true} value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                <CustomInput id='password' type='password' autoComplete='current-password' name='password' labelText='Password' required={true} value={password} onChange={(e)=>setPassword(e.target.value)}/>
-                <Button className="my-2" type='submit' variant="primary">
-                    Sign In
-                </Button>    
-            </Form>
-            <Row className="py-3">
-                <Col>
-                    New Customer? <Link to={redirect ? `/register?redirect=${redirect}`:'/register'}>Register</Link>
-                </Col>    
-            </Row>
-        </FormContainer>
-    </div>
-  );
+	const submitHandler = (e) => {
+		e.preventDefault();
+		dispatch(login(email, password));
+	};
+
+	return (
+		<div className='Login'>
+			<FormContainer>
+				<h1>Sign In</h1>
+				{error && <Message variant='danger'>{error}</Message>}
+				{loading && <Loader />}
+				<MetaWrapper title='TAA-Login' />
+				<Form onSubmit={submitHandler}>
+					<CustomInput
+						id='email'
+						type='email'
+						autoComplete='current-email'
+						name='email'
+						labelText='Email Address'
+						required={true}
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<CustomInput
+						id='password'
+						type='password'
+						autoComplete='current-password'
+						name='password'
+						labelText='Password'
+						required={true}
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<Button className='my-2' type='submit' variant='primary'>
+						Sign In
+					</Button>
+				</Form>
+				<Row className='py-3'>
+					<Col>
+						New Customer?{' '}
+						<Link
+							to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+							Register
+						</Link>
+					</Col>
+				</Row>
+				<Row className='py-3'>
+					<Col>
+						Demo user account - Email Address: demo@demo.com Password: demouser
+					</Col>
+				</Row>
+				<Row className='py-3'>
+					<Col>
+						Admin user account - Email Address: admin@admin.com Password: 123456
+					</Col>
+				</Row>
+			</FormContainer>
+		</div>
+	);
 };
 
 export default Login;
