9dd109a0bf32a967a50603449585a9e87eecac34
SUBstylee <jjthrelfall@gmail.com>
2024-10-01 14:00:15 +0800

    changed endpoint urls

--- frontend/src/pages/Order/Order.jsx
+++ frontend/src/pages/Order/Order.jsx
@@ -51,7 +51,7 @@ const Order = () => {
           };
 
         const addPayPalScript=async ()=>{
-            const {data:clientId}=await axios.get('/api/config/paypal');
+            const {data:clientId}=await axios.get('https://taa-nke7.onrender.com/api/config/paypal');
             const script=document.createElement('script');
             script.type='text/javascript';
             script.src=`https://www.paypal.com/sdk/js?client-id=${clientId}`;
